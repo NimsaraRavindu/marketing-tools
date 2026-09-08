0efe3e5867d92438d783f82d8b3bd55023983810
Nimsara Aluthgedara <nimsaraa@nimsaraa.local>
2026-09-08 11:54:04 +0530

    feat(registrant): add /registrant api gateway routing prefix

--- apps/registrant/backend/cmd/server/main.go
+++ apps/registrant/backend/cmd/server/main.go
@@ -119,7 +119,7 @@ func main() {
 	r := gin.New()
 	r.Use(gin.Recovery())
 
-	api := r.Group("")
+	api := r.Group("/registrant")
 	api.Use(middleware.JwtInterceptor())
 	{
 		api.GET("/events/current/agendas", agendaH.ListCurrentAgendas)
